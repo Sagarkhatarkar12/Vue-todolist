5051211f5a84aeded7ccc8d1868db42d155811df
SDB <91771020+Sagarkhatarkar12@users.noreply.github.com>
2025-05-19 18:34:43 +0530

    basic node first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import Form from './components/form.vue';
+import List from './components/List.vue';
+export default {
+  components:{
+    Form,
+    List,
+  },
+  data(){
+return {
+  // data part 
+  newTodo :"Hiii sagar khatarkar"
+}
+},
+methods:{
+  writeText(){
+    this.newTodo = "Hii I am Sagu";
+  }
+}
+};
+
+</script>
+
+<template>
+  <div>
+    <Form />
+    <List/>
+
+ 
+
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>{{ message }}</h1>
+</template>
+<script>
+export default {
+    data(){
+    return {
+        message :"List render here "
+    }
+    },
+    methods :{
+
+    }
+}
+
+</script>
+<style>
+
+</style>
